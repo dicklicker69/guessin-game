c44d15c9aa06b87e04cebe962d4462b3473e5ccd
ojn <99439989+dicklicker69@users.noreply.github.com>
2022-02-15 01:17:12 +0000

    fuicki uoff

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-import random
+import random #imports random number
 
-MIN_NUMBER = 1
-MAX_NUMBER = 20 
+MIN_NUMBER = 1    #Minimum number
+MAX_NUMBER = 20   #Maximum number
 
-random_number = int(random.randint(MIN_NUMBER, MAX_NUMBER))
+random_number = int(random.randint(MIN_NUMBER, MAX_NUMBER)) 
+#Makes random number a value
 
-print(random_number)
+print(random_number) # Prints random number for error checking will not be in final ver
 
 while True: 
 
     user_answer = int(input('guess a number bettween {} and {}  : '.format(MIN_NUMBER, MAX_NUMBER)))
+  #gives user answer a value and asks main question
 
     if(user_answer == random_number): 
         print('you have guessed the number !!!!')
         break
+      #Ends game if user answer is correct
     
     elif(user_answer > random_number): 
-        print('that number is too big')
-
+       print('that number is too big')
+  #Gives user another chance at answering and also tells them the number is to big
+  
     elif(user_answer < random_number): 
         print('that number is too small')
+  #Gives user another chance at answering and also tells them the number is to small
+
+    
+  
+      
+
+
+
+
+
+
